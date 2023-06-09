7fc4308527b214aee8635491365d052573b0f03e
maulanazh15 <maulanazh15@gmail.com>
2023-06-09 09:59:55 +0700

    ganti tampilan dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,13 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\Barang;
 use Illuminate\Http\Request;
+use App\Models\Supplier;
+use App\Models\Klien;
+
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        $total_klien = klien::count();
+        $total_supplier = Supplier::count();
         $total_data_barang = Barang::count();
         $total_stok_barang = Barang::sum('stok');
-        return view('dashboard.index', compact('total_data_barang', 'total_stok_barang'));
+        return view('dashboard.index', compact('total_data_barang', 'total_stok_barang', 'total_klien', 'total_supplier'));
     }
 }
